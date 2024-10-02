a2eaa7513aa1b4d3a78aa5819f55569fd65e316c
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-10-02 13:02:06 +0200

    food items decoration

---
+++ src/foods/dto/index.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+
+export class CreateFoodDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @ApiProperty()
+  @IsInt()
+  @IsNotEmpty()
+  category_id: number;
+}
+
+export class UpdateFoodDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @ApiProperty()
+  @IsInt()
+  @IsNotEmpty()
+  category_id: number;
+}
+class Food {
+  @ApiProperty()
+  created_at: Date;
+  @ApiProperty()
+  updated_at: Date;
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  category_id: number;
+}
+export class CreateFoodResponse extends Food {}
+export class FindAllFoodsResponse extends Food {}
+export class FindOneFoodResponse extends Food {}
+export class UpdateFoodResponse extends Food {}
+export class RemoveFoodResponse extends Food {}
--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -8,34 +8,63 @@ import {
   Delete,
 } from '@nestjs/common';
 import { FoodsService } from './foods.service';
-import { CreateFoodDto } from './dto/create-food.dto';
-import { UpdateFoodDto } from './dto/update-food.dto';
+import {
+  CreateFoodDto,
+  CreateFoodResponse,
+  FindAllFoodsResponse,
+  FindOneFoodResponse,
+  RemoveFoodResponse,
+  UpdateFoodDto,
+  UpdateFoodResponse,
+} from './dto';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Food Items')
 @Controller('foods')
 export class FoodsController {
   constructor(private readonly foodsService: FoodsService) {}
 
   @Post()
+  @ApiOperation({
+    operationId: 'createFoodItem',
+  })
+  @ApiOkResponse({ type: CreateFoodResponse })
   create(@Body() createFoodDto: CreateFoodDto) {
     return this.foodsService.create(createFoodDto);
   }
 
   @Get()
+  @ApiOperation({
+    operationId: 'findAllFoodItems',
+  })
+  @ApiOkResponse({ type: FindAllFoodsResponse, isArray: true })
   findAll() {
     return this.foodsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    operationId: 'findOneFoodItem',
+  })
+  @ApiOkResponse({ type: FindOneFoodResponse })
   findOne(@Param('id') id: string) {
     return this.foodsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    operationId: 'updateFoodItem',
+  })
+  @ApiOkResponse({ type: UpdateFoodResponse })
   update(@Param('id') id: string, @Body() updateFoodDto: UpdateFoodDto) {
     return this.foodsService.update(+id, updateFoodDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    operationId: 'removeFoodItem',
+  })
+  @ApiOkResponse({ type: RemoveFoodResponse })
   remove(@Param('id') id: string) {
     return this.foodsService.remove(+id);
   }
