39f7ce148554808470203acdc21b460d13a68234
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-10-02 12:47:53 +0200

    food categories decoration

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -8,29 +8,55 @@ import {
   Delete,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import {
+  CreateCategoryDto,
+  CreateCategoryResponse,
+  FindAllCategoriesResponse,
+  FindOneCategoryResponse,
+  RemoveCategoryResponse,
+  UpdateCategoryDto,
+  UpdateCategoryResponse,
+} from './dto';
 
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Food Categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
+  @ApiOperation({
+    operationId: 'createFoodCategory',
+  })
+  @ApiOkResponse({ type: CreateCategoryResponse })
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
   }
 
   @Get()
+  @ApiOperation({
+    operationId: 'findAllFoodCategories',
+  })
+  @ApiOkResponse({ type: FindAllCategoriesResponse, isArray: true })
   findAll() {
     return this.categoriesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    operationId: 'findOneFoodCategory',
+  })
+  @ApiOkResponse({ type: FindOneCategoryResponse })
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    operationId: 'updateFoodCategory',
+  })
+  @ApiOkResponse({ type: UpdateCategoryResponse })
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -39,6 +65,10 @@ export class CategoriesController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    operationId: 'removeFoodCategory',
+  })
+  @ApiOkResponse({ type: RemoveCategoryResponse })
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(+id);
   }
